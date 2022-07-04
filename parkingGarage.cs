fe15fc934a295b9a2929aceeafc68d4e51a7f76b
sameenshafeeq <sameenshafeeq123@gmail.com>
2022-07-03 16:01:24 -0800

    Add files via upload

---
+++ parkingGarage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPaarkingBill
+{  
+    internal class parkingGarage
+    {
+
+       private string Name;
+       private List<vehicles> vehicles = new List<vehicles>();
+        public parkingGarage(string name)
+        {
+            Name = name;
+        }
+        public void setName(string name)
+        {
+            Name = name;
+        }
+        public string getNmae()
+        {
+            return Name;
+        }
+
+    }
+}
